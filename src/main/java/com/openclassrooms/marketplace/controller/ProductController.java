c8e9d71e9533fbbd82373311fa7ca5eeaf969de8
David <zair.d@outlook.fr>
2021-06-05 12:55:48 -0400

    feat(service): add DAO and it's implementation, update controller

--- src/main/java/com/openclassrooms/marketplace/controller/ProductController.java
+++ src/main/java/com/openclassrooms/marketplace/controller/ProductController.java
@@ -1,12 +1,25 @@
 package com.openclassrooms.marketplace.controller;
 
+import com.openclassrooms.marketplace.dao.ProductDao;
 import com.openclassrooms.marketplace.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class ProductController {
+
+    @Autowired
+    private ProductDao productDao;
+
+    //Products
+    @GetMapping(value = "Products")
+    public List<Product> productList() {
+        return productDao.findAll();
+    }
 
     //Produits/{id}
     @GetMapping(value = "Products/{id}")
